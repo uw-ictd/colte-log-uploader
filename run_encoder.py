00aeb11fc4cef333beb4fd097135d4062a596d63
Matt Johnson <matt9j@cs.washington.edu>
2019-05-15 13:48:30 -0700

    [run] Allow storing the key in a keyfile
    Allow reading a keyfile instead of directly specifying the key
    material on the command line. This helps usable security, since the
    command line is visible at other places in the OS (process list,
    scrollback, etc.)

--- run_encoder.py
+++ run_encoder.py
@@ -12,8 +12,11 @@ DNS_OUT_FILE = "dns_archive"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("key",
-                        help="The key string to use when encoding data.")
+    key_group = parser.add_mutually_exclusive_group(required=True)
+    key_group.add_argument("-f", "--keyfile",
+                           help="The file holding the encoding key.")
+    key_group.add_argument("-k", "--key",
+                           help="The key string to use when encoding data.")
     args = parser.parse_args()
 
     if not PASS or PASS is None:
@@ -27,8 +30,13 @@ if __name__ == "__main__":
                                          db_password=db_password,
                                          )
 
-    encoder = colte.log_tools.encoder.StreamingEncoder(reader,
-                                                       args.key.encode('utf8'))
+    if args.keyfile is not None:
+        with open(args.keyfile, mode="rb") as f:
+            key = f.read()
+    else:
+        key = args.key.encode('utf8')
+
+    encoder = colte.log_tools.encoder.StreamingEncoder(reader, key)
     encoder.stream_flowlogs_to_file(FLOWLOG_OUT_FILE)
     encoder.stream_dns_to_file(DNS_OUT_FILE)
     reader.close()
