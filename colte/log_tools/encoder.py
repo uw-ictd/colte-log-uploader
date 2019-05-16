53c3198a2f72d826c70b79730ae914c82a68ced7
Matt Johnson <matt9j@cs.washington.edu>
2019-05-16 09:19:01 -0700

    [log_tools] Refactor encoding to separate functions

--- colte/log_tools/encoder.py
+++ colte/log_tools/encoder.py
@@ -25,6 +25,84 @@ class StreamingEncoder(object):
 
         return ip_to_id
 
+    def _encode_flowlog(self, flowlog):
+        row_fields = dict()
+        row_fields["start_time"] = flowlog[0]
+        row_fields["end_time"] = flowlog[1]
+        if len(flowlog[2]) == 16:
+            address_a = ipaddress.IPv6Address(bytes(flowlog[2]))
+            address_b = ipaddress.IPv6Address(bytes(flowlog[3]))
+        elif len(flowlog[2]) == 4:
+            address_a = ipaddress.IPv4Address(bytes(flowlog[2]))
+            address_b = ipaddress.IPv4Address(bytes(flowlog[3]))
+        else:
+            raise ValueError("IP length is invalid")
+
+        # Handle Address Anonymization
+        if address_a in self._coded_ids.keys():
+            row_fields["obfuscated_a"] = self._coded_ids[address_a]
+        else:
+            row_fields["address_a"] = address_a
+
+        if address_b in self._coded_ids.keys():
+            row_fields["obfuscated_b"] = self._coded_ids[address_b]
+        else:
+            row_fields["address_b"] = address_b
+
+        row_fields["transport_protocol"] = flowlog[4]
+        row_fields["port_a"] = flowlog[5]
+        row_fields["port_b"] = flowlog[6]
+        row_fields["bytes_a_to_b"] = flowlog[7]
+        row_fields["bytes_b_to_a"] = flowlog[8]
+
+    def _encode_dns(self, raw_log):
+        # Convert to ipaddress types
+        if len(raw_log[1]) == 4:
+            src_addr = ipaddress.IPv4Address(bytes(raw_log[1]))
+        else:
+            src_addr = ipaddress.IPv6Address(bytes(raw_log[1]))
+
+        if len(raw_log[2]) == 4:
+            dst_addr = ipaddress.IPv4Address(bytes(raw_log[2]))
+        else:
+            dst_addr = ipaddress.IPv6Address(bytes(raw_log[2]))
+
+        row_fields = {"timestamp": raw_log[0],
+                      "src_ip": src_addr,
+                      "dst_ip": dst_addr,
+                      "protocol": raw_log[3],
+                      "src_port": raw_log[4],
+                      "dst_port": raw_log[5],
+                      "opcode": raw_log[6],
+                      "resultcode": raw_log[7],
+                      "host": raw_log[8],
+                      "response_addresses": list(),
+                      "response_ttls": list(),
+                      "answer_index": raw_log[11],
+                      }
+
+        # Parse the variable number of response addresses and ttls
+        addresses = raw_log[9].split(",")
+        ttls = raw_log[10].split(",")
+        for address, ttl in zip(addresses, ttls):
+            if address != '':
+                if ttl == '':
+                    raise ValueError("Mismatched number of address and ttl")
+                row_fields["response_addresses"].append(
+                    ipaddress.ip_address(address))
+                row_fields["response_ttls"].append(ttl)
+
+        # Obfuscate any local addresses:
+        if src_addr in self._coded_ids.keys():
+            row_fields["obfuscated_src"] = self._coded_ids[src_addr]
+            del row_fields["src_ip"]
+
+        if dst_addr in self._coded_ids.keys():
+            row_fields["obfuscated_dst"] = self._coded_ids[dst_addr]
+            del row_fields["dst_ip"]
+
+        return row_fields
+
     def stream_flowlogs_to_file(self, filename):
         with open(filename, 'ab') as f:
             print("Beginning Flow Recording")
@@ -34,35 +112,8 @@ class StreamingEncoder(object):
                     if i % 10000 == 0:
                         print("Reached row", i)
 
-                    row_fields = dict()
-                    row_fields["start_time"] = row[0]
-                    row_fields["end_time"] = row[1]
-                    if len(row[2]) == 16:
-                        address_a = ipaddress.IPv6Address(bytes(row[2]))
-                        address_b = ipaddress.IPv6Address(bytes(row[3]))
-                    elif len(row[2]) == 4:
-                        address_a = ipaddress.IPv4Address(bytes(row[2]))
-                        address_b = ipaddress.IPv4Address(bytes(row[3]))
-                    else:
-                        raise ValueError("IP length is invalid")
-
-                    # Handle Address Anonymization
-                    if address_a in self._coded_ids.keys():
-                        row_fields["obfuscated_a"] = self._coded_ids[address_a]
-                    else:
-                        row_fields["address_a"] = address_a
-
-                    if address_b in self._coded_ids.keys():
-                        row_fields["obfuscated_b"] = self._coded_ids[address_b]
-                    else:
-                        row_fields["address_b"] = address_b
-
-                    row_fields["transport_protocol"] = row[4]
-                    row_fields["port_a"] = row[5]
-                    row_fields["port_b"] = row[6]
-                    row_fields["bytes_a_to_b"] = row[7]
-                    row_fields["bytes_b_to_a"] = row[8]
-                    pickle.dump(row_fields, f)
+                    encoded_log = self._encode_flowlog(row)
+                    pickle.dump(encoded_log, f)
 
     def stream_dns_to_file(self, filename):
         with open(filename, 'ab') as f:
@@ -73,49 +124,5 @@ class StreamingEncoder(object):
                     if i % 10000 == 0:
                         print("Reached row", i)
 
-                    # Convert to ipaddress types
-                    if len(row[1]) == 4:
-                        src_addr = ipaddress.IPv4Address(bytes(row[1]))
-                    else:
-                        src_addr = ipaddress.IPv6Address(bytes(row[1]))
-
-                    if len(row[2]) == 4:
-                        dst_addr = ipaddress.IPv4Address(bytes(row[2]))
-                    else:
-                        dst_addr = ipaddress.IPv6Address(bytes(row[2]))
-
-                    row_fields = {"timestamp": row[0],
-                                  "src_ip": src_addr,
-                                  "dst_ip": dst_addr,
-                                  "protocol": row[3],
-                                  "src_port": row[4],
-                                  "dst_port": row[5],
-                                  "opcode": row[6],
-                                  "resultcode": row[7],
-                                  "host": row[8],
-                                  "response_addresses": list(),
-                                  "response_ttls": list(),
-                                  "answer_index": row[11],
-                                  }
-
-                    # Parse the variable number of response addresses and ttls
-                    addresses = row[9].split(",")
-                    ttls = row[10].split(",")
-                    for address, ttl in zip(addresses, ttls):
-                        if address != '':
-                            if ttl == '':
-                                raise ValueError("Mismatched number of address and ttl")
-                            row_fields["response_addresses"].append(
-                                ipaddress.ip_address(address))
-                            row_fields["response_ttls"].append(ttl)
-
-                    # Obfuscate any local addresses:
-                    if src_addr in self._coded_ids.keys():
-                        row_fields["obfuscated_src"] = self._coded_ids[src_addr]
-                        del row_fields["src_ip"]
-
-                    if dst_addr in self._coded_ids.keys():
-                        row_fields["obfuscated_dst"] = self._coded_ids[dst_addr]
-                        del row_fields["dst_ip"]
-
-                    pickle.dump(row_fields, f)
+                    encoded_row = self._encode_dns(row)
+                    pickle.dump(encoded_row, f)
