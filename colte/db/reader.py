76136a58746b9675c428b167948efb372ffb349b
Matt Johnson <matt9j@cs.washington.edu>
2019-05-16 17:11:17 -0700

    [log_tools] Create accessor methods for flowlog staging area

--- colte/db/reader.py
+++ colte/db/reader.py
@@ -35,6 +35,42 @@ class ColteReader(object):
         def __next__(self):
             return self._cursor.__next__()
 
+    def stage_flow_logs(self):
+        self._cnx.start_transaction(isolation_level="SERIALIZABLE")
+        try:
+            cursor = self._cnx.cursor()
+            cursor.execute("drop table flowlogs_staging; "
+                           "create table flowlogs_staging "
+                           "as (select * from flowlogs);")
+            self._cnx.commit()
+        except Exception as e:
+            self._cnx.rollback()
+            raise e
+
+        return self._CursorIterator(connection=self._cnx,
+                                    query="select * from flowlogs_staging;")
+
+    def purge_currently_staged_flowlogs(self):
+        self._cnx.start_transaction(isolation_level="SERIALIZABLE")
+        try:
+            cursor = self._cnx.cursor()
+            cursor.execute("DELETE FROM flowlogs "
+                           "WHERE EXISTS ("
+                           "SELECT * FROM flowlogs, flowlogs_staging as staging"
+                           "WHERE flowlogs.intervalStart = staging.intervalStart "
+                           "AND flowlogs.intervalStop = staging.intervalStop "
+                           "AND flowlogs.addressA = staging.addressA "
+                           "AND flowlogs.addressB = staging.addressB "
+                           "AND flowlogs.transportProtocol = staging.transportProtocol "
+                           "AND flowlogs.portA = staging.portA "
+                           "AND flowlogs.portB = staging.portB "
+                           "AND flowlogs.bytesAtoB = staging.bytesAtoB "
+                           "AND flowlogs.bytesBtoA = staging.bytesBtoA);")
+            self._cnx.commit()
+        except Exception as e:
+            self._cnx.rollback()
+            raise e
+
     def flow_logs(self):
         return self._CursorIterator(connection=self._cnx,
                                     query="select * from flowlogs;")
